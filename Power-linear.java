0cb8bd07a5b7a854470d80b9dd6bb90edca766ee
shubham Chauhan <33725542+shu0718@users.noreply.github.com>
2021-01-25 11:06:37 +0530

    Create Power-linear.java

---
+++ Power-linear.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Shubham
+{
+  public static void main(String[] args)
+  {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int fact = factorial(n);
+      System.out.println(fact);
+  }
+  public static int factorial(int n)
+  {
+      if(n==0 || n==1){
+          return 1;
+      }
+      return n*fatorial(n-1);
+  }
+}  
+
+
+INPUT:-
+2
+5
+OUTPUT:-
+32
