c06f52dcc128f13061293db436da854f968fc701
shubham Chauhan <33725542+shu0718@users.noreply.github.com>
2021-01-25 10:31:47 +0530

    Create print decreasing and increasing order of sequences.java

---
+++ print decreasing and increasing order of sequences.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Shubham 
+{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pdi(n);
+    }
+    public static void pdi(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
+}
+
+
+INPUT:-
+4
+OUTPUT:-
+4
+3
+2
+1
+1
+2
+3
+4
